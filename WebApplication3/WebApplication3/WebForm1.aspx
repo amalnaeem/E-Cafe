f19f655c61060d6904f1cf54ec7b6006b9566fc9
amalnaeem <36226153+amalnaeem@users.noreply.github.com>
2018-02-21 16:52:15 +0500

    Version1

---
+++ WebApplication3/WebApplication3/WebForm1.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication3.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #form1 {
+            height: 647px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; SofTechno Restaurant
+        <br />
+        <br />
+        <br />
+&nbsp;
+        <br />
+&nbsp;&nbsp;
+        <asp:ListBox ID="ListBox1" runat="server" Height="108px" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged" AutoPostBack="True" Width="120px"></asp:ListBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:ListBox ID="ListBox2" runat="server" Height="108px" Width="120px" OnSelectedIndexChanged="ListBox2_SelectedIndexChanged" AutoPostBack="True"></asp:ListBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:ListBox ID="ListBox3" runat="server" Height="108px" OnSelectedIndexChanged="ListBox3_SelectedIndexChanged" AutoPostBack="True" Width="120px"></asp:ListBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:ListBox ID="ListBox4" runat="server" Height="108px" Width="120px" OnSelectedIndexChanged="ListBox4_SelectedIndexChanged" AutoPostBack="True"></asp:ListBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:ListBox ID="ListBox5" runat="server" Height="108px" Width="120px" OnSelectedIndexChanged="ListBox5_SelectedIndexChanged" AutoPostBack="True"></asp:ListBox>
+        <br />
+        <br />
+        <asp:ListBox ID="ListBox6" runat="server" OnSelectedIndexChanged="ListBox6_SelectedIndexChanged" style="margin-left: 15px" Visible="False" Width="115px"></asp:ListBox>
+        <br />
+    </form>
+</body>
+</html>
---
+++ WebApplication3/WebApplication3/WebForm1.aspx.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace WebApplication3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        protected override void OnLoad(EventArgs e)
+        {
+           
+            base.OnLoad(e);
+
+            if (!IsPostBack)
+            {
+
+                System.Data.OleDb.OleDbConnection conn = new System.Data.OleDb.OleDbConnection();
+                conn.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;" +
+            @"Data source= C:\Users\OK\Documents\Ecafe.accdb";
+
+                conn.Open();
+
+                String my_querry1 = "select DISTINCT Type from Items where Category = 'Starter'";
+                String my_querry2 = "select DISTINCT Type from Items where Category = 'Main'";
+                String my_querry3 = "select DISTINCT Type from Items where Category = 'Deserts'";
+                String my_querry4 = "select DISTINCT Type from Items where Category = 'Salad'";
+                String my_querry5 = "select DISTINCT Type from Items where Category = 'Drinks'";
+                OleDbCommand cmd1 = new OleDbCommand(my_querry1, conn);
+                OleDbCommand cmd2 = new OleDbCommand(my_querry2, conn);
+                OleDbCommand cmd3 = new OleDbCommand(my_querry3, conn);
+                OleDbCommand cmd4 = new OleDbCommand(my_querry4, conn);
+                OleDbCommand cmd5 = new OleDbCommand(my_querry5, conn);
+
+                //OleDbCommand cmd2 = new OleDbCommand(my_querry2, conn);
+                //OleDbCommand cmd3 = new OleDbCommand(my_querry3, conn);
+                //OleDbDataReader dr = cmd.ExecuteReader();
+                var dr1 = cmd1.ExecuteReader();
+
+                // MessageBox.Show("Query executed");
+                while (dr1.Read())
+                {
+                    ListBox1.Items.Add(dr1[0].ToString());
+                    //MessageBox.Show(dr1[1].ToString());
+                }
+
+                dr1 = cmd2.ExecuteReader();
+
+                // MessageBox.Show("Query executed");
+                while (dr1.Read())
+                {
+                    ListBox2.Items.Add(dr1[0].ToString());
+                    //MessageBox.Show(dr1[1].ToString());
+                }
+
+                dr1 = cmd3.ExecuteReader();
+
+                // MessageBox.Show("Query executed");
+                while (dr1.Read())
+                {
+                    ListBox3.Items.Add(dr1[0].ToString());
+                    //MessageBox.Show(dr1[1].ToString());
+                }
+
+                dr1 = cmd4.ExecuteReader();
+
+                // MessageBox.Show("Query executed");
+                while (dr1.Read())
+                {
+                    ListBox4.Items.Add(dr1[0].ToString());
+                    //MessageBox.Show(dr1[1].ToString());
+                }
+
+                dr1 = cmd5.ExecuteReader();
+
+                // MessageBox.Show("Query executed");
+                while (dr1.Read())
+                {
+                    ListBox5.Items.Add(dr1[0].ToString());
+                    //MessageBox.Show(dr1[1].ToString());
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            ListBox6.Visible = true;
+        }
+        protected void ListBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show("hi0");
+            ListBox6.Visible = true;
+        }
+
+        protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox6.Visible = true;
+        }
+
+        protected void ListBox6_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ListBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox6.Visible = true;
+        }
+
+        protected void ListBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox6.Visible = true;
+        }
+
+        protected void ListBox5_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox6.Visible = true;
+        }
+    }
+}
