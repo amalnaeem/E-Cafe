54a9bebeb8cada79efefe68faefdc3000d1e1ebc
amalnaeem <36226153+amalnaeem@users.noreply.github.com>
2018-02-23 20:16:54 +0500

    Updated DB a

---
+++ WebApplication3/WebApplication3/WebForm2.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="WebApplication3.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:CheckBoxList ID="CheckBoxList1" runat="server" OnSelectedIndexChanged="CheckBoxList1_SelectedIndexChanged">
+            </asp:CheckBoxList>
+            <asp:CheckBoxList ID="CheckBoxList2" runat="server" style="margin-bottom: 0px">
+            </asp:CheckBoxList>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication3/WebApplication3/WebForm2.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+
+namespace WebApplication3
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+
+            base.OnLoad(e);
+
+            if (!IsPostBack)
+            {
+
+                System.Data.OleDb.OleDbConnection conn = new System.Data.OleDb.OleDbConnection();
+                conn.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;" +
+            @"Data source= C:\Users\OK\Documents\Ecafe.accdb";
+
+                conn.Open();
+
+                String my_querry1 = "select ID from Orders where OrderType = 'Delivery'";
+                OleDbCommand cmd1 = new OleDbCommand(my_querry1, conn);
+
+                var dr1 = cmd1.ExecuteReader();
+
+                // MessageBox.Show("Query executed");
+                while (dr1.Read())
+                {
+                    CheckBoxList1.Items.Add(dr1[0].ToString());
+                    //MessageBox.Show(dr1[1].ToString());
+                }
+
+            }
+        }
+
+                protected void CheckBoxList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
